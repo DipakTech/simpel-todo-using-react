c9997f4fd894d4d146775d525f3ff77c122f5b6a
Dipak giri <dipakgiri1200@gmail.com>
2021-07-18 09:13:11 +0545

    todo add feature build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 export default class App extends Component {
   state = {
@@ -16,11 +17,19 @@ export default class App extends Component {
       todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos,
+    })
+  }
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     )
   }
