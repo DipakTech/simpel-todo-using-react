5a97645c8bb4aa6b3389721bd7da6a2815b1132a
Dipak giri <dipakgiri1200@gmail.com>
2021-07-20 13:00:39 +0545

    setting individual post and detains page of each post

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react'
-import Todos from './Todos'
-import AddTodo from './AddTodo'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route,Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
+import Post from './components/post'
 
 export default class App extends Component {
-  state = {
-    todos: [
-      { id: 1, content: 'buy some milk' },
-      {id:2,content:'play mariocard'}
-    ]
-  }
-  deleteTodo = (id) => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    });
-    this.setState({
-      todos
-    })
-  }
-  addTodo = (todo) => {
-    todo.id = Math.random();
-    let todos = [...this.state.todos, todo];
-    this.setState({
-      todos: todos,
-    })
-  }
   render() {
     return (
-      <div className="todo-app container">
-        <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo}/>
-      </div>
+      <BrowserRouter>
+      <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/contact' component={Contact}/>
+            <Route path='/about' component={About} />
+            <Route path="/:post_id" component={Post}/>
+          </Switch>
+     </div>
+      </BrowserRouter>
     )
   }
 }
